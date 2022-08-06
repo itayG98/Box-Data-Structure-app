e98ab71633012b0497a31d9d477f13f2ad2997b7
GEITAY5@365.openu.ac.il <GEITAY5@365.openu.ac.il>
2022-08-06 16:20:22 +0300

    break

--- DataStructure/Queue.cs
+++ DataStructure/Queue.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace DataStructure
 {
-    public class Queue
+    public class Queue<V> where V : IComparable
     {
+        QueueNode Root;
+        public Queue(QueueNode root)
+        {
+            Root = root;
+        }
+        public Queue() : this(null)
+        {
+        }
+
+        public bool IsEmpty() => Root == null;
+
+        public class QueueNode 
+        {
+            private QueueNode _next;
+            private V _value;
+
+            public V Value { get => _value; set => _value = value; }
+            public QueueNode Next { get => _next; set => _next = value; }
+            public int CompareTo(V val) => Value.CompareTo(val);
+        }
     }
 }
